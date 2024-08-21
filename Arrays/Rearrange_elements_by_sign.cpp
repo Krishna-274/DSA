ab59a03ecc975f247f307db2a169f9b645c45354
Krishna Priya <138705417+Krishna-274@users.noreply.github.com>
2024-08-21 23:19:26 +0530

    Create Rearrange_elements_by_sign.cpp

---
+++ Arrays/Rearrange_elements_by_sign.cpp
@@ -0,0 +1,119 @@
+//variety1
+//Brute force 
+  vector<int> rearrangeArray(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>pos;
+         vector<int>neg;
+       for(int i=0;i<n;i++){
+          if(nums[i]>0)
+          pos.push_back(nums[i]);
+          else
+          neg.push_back(nums[i]);
+        }
+        for(int i=0;i<n/2;i++){
+            nums[2*i]=pos[i];
+             nums[2*i+1]=neg[i];
+        }
+       
+        return nums;
+    }
+
+
+//optimal
+ vector<int> RearrangebySign(vector<int>A){
+    
+  int n = A.size();
+  
+  // Define array for storing the ans separately.
+  vector<int> ans(n,0);
+  
+  // positive elements start from 0 and negative from 1.
+  int posIndex = 0, negIndex = 1;
+  for(int i = 0;i<n;i++){
+      
+      // Fill negative elements in odd indices and inc by 2.
+      if(A[i]<0){
+          ans[negIndex] = A[i];
+          negIndex+=2;
+      }
+      
+      // Fill positive elements in even indices and inc by 2.
+      else{
+          ans[posIndex] = A[i];
+          posIndex+=2;
+      }
+  }
+  
+  return ans;
+    
+}
+
+
+//variet2
+//There’s an array ‘A’ of size ‘N’ with positive and negative elements (not necessarily equal).
+//Without altering the relative order of positive and negative elements, you must return an array of alternately
+// positive and negative values. The leftover elements should be placed at the very end in the same order as in array A.
+
+//approach
+vector<int> RearrangebySign(vector<int>A, int n){
+    
+  // Define 2 vectors, one for storing positive 
+  // and other for negative elements of the array.
+  vector<int> pos;
+  vector<int> neg;
+  
+  // Segregate the array into positives and negatives.
+  for(int i=0;i<n;i++){
+      
+      if(A[i]>0) pos.push_back(A[i]);
+      else neg.push_back(A[i]);
+  }
+  
+  // If positives are lesser than the negatives.
+  if(pos.size() < neg.size()){
+      
+    // First, fill array alternatively till the point 
+    // where positives and negatives ar equal in number.
+    for(int i=0;i<pos.size();i++){
+      
+      A[2*i] = pos[i];
+      A[2*i+1] = neg[i];
+    }
+    
+    // Fill the remaining negatives at the end of the array.
+    int index = pos.size()*2;
+    for(int i = pos.size();i<neg.size();i++){
+        
+        A[index] = neg[i];
+        index++;
+    }
+  }
+  
+  // If negatives are lesser than the positives.
+  else{
+      
+      // First, fill array alternatively till the point 
+      // where positives and negatives ar equal in number.
+      for(int i=0;i<neg.size();i++){
+      
+      A[2*i] = pos[i];
+      A[2*i+1] = neg[i];
+  }
+    
+    // Fill the remaining positives at the end of the array.
+    int index = neg.size()*2;
+    for(int i = neg.size();i<pos.size();i++){
+        
+        A[index] = pos[i];
+        index++;
+    }
+  }
+  return A;
+    
+}
+
+//Time Complexity: O(2*N)
+//space complexity:O(N)
+
+
+
