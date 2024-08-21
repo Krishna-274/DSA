37fba418c1627ea30189a4185348f3be40623c62
Krishna Priya <138705417+Krishna-274@users.noreply.github.com>
2024-08-21 18:05:48 +0530

    Update Majority element.cpp

--- Arrays/Majority element.cpp
+++ Arrays/Majority element.cpp
@@ -1,4 +1,4 @@
-
+//Brute force
    int majorityElement(vector<int>& nums) {
        int n=nums.size();
        for(int i=0;i<n;i++)
@@ -15,3 +15,52 @@
    
     return -1;
    }
+
+
+
+//Better approach
+int majorityElement(vector<int>& nums) {
+        int n=nums.size();
+       map<int ,int>mpp;
+        for(int i=0;i<n;i++){
+            mpp[nums[i]]++;
+        }
+        for(auto it:mpp){
+        if(it.second>n/2)
+        return it.first;
+    }
+    return -1;}
+
+
+
+
+
+//Optimal approach--MOORE'S VOTING ALGORITHM
+int majorityElement(vector<int> v) {
+
+    //size of the given array:
+    int n = v.size();
+    int cnt = 0; // count
+    int el; // Element
+
+    //applying the algorithm:
+    for (int i = 0; i < n; i++) {
+        if (cnt == 0) {
+            cnt = 1;
+            el = v[i];
+        }
+        else if (el == v[i]) cnt++;
+        else cnt--;
+    }
+
+    //checking if the stored element
+    // is the majority element:
+   //this loop not required if prblm states that there definitely exists a majority element
+    int cnt1 = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == el) cnt1++;
+    }
+
+    if (cnt1 > (n / 2)) return el;
+    return -1;
+}
